39a3a218d98807aab1504c101fb5230a95f77034
khoirulanam20 <khrlanm02@gmail.com>
2026-06-30 12:22:04 +0700

    update app name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -276,7 +276,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Bintang Kecil Daycare</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
